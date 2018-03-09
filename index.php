41b9cdce874d2edba2c17b12502f8880ab16270c
lgarciacarmona <lgarciacarmona@users.noreply.github.com>
2018-03-08 21:12:14 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+  $variable1 = 32;
+
+   // if - else (SI_NO)
+
+  if ($variable1 == 32) {
+
+  	echo ("son iguales ");
+  	
+  }else{
+    echo ("no son iguales ");
+ }
+?><br>
+echo "-___________________";
+<?php
+  ///while (mientras)
+  $indice = 0;
+  while ($indice <= 15) {
+  	echo " soy el numero: ".$indice;
+  	$indice = $indice +1;
+
+  };
+?><br>
+<?php
+echo "-___________________";
+  //sentencia for
+  for ($indice2=0; $indice2 <=10 ; $indice2++) { 
+    echo "soy otro numero  ".$indice2." ";
+  } 
+
+?><br>
